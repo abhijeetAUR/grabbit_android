ab95690101269528d21d184313e5bc19359ea72f
abhijeetAUR <abhijeetr131@gmail.com>
2019-09-16 23:56:28 +0530

    change color of login signup button navigation code added in login and signup activity

--- grabbit/app/src/main/java/com/example/grabbit/LoginActivity.kt
+++ grabbit/app/src/main/java/com/example/grabbit/LoginActivity.kt
@@ -1,12 +1,28 @@
 package com.example.grabbit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.grabbit.home.HomeActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        button.setOnClickListener {
+            // Handler code here.
+            val intent = Intent(applicationContext, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        textView3.setOnClickListener {
+            val intent = Intent(applicationContext, SignupActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- grabbit/app/src/main/java/com/example/grabbit/SignupActivity.kt
+++ grabbit/app/src/main/java/com/example/grabbit/SignupActivity.kt
@@ -1,12 +1,23 @@
 package com.example.grabbit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.grabbit.home.HomeActivity
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_login.button
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        button.setOnClickListener {
+            // Handler code here.
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
