7ebfd27bd8c99add26e3fe72fdbd33a17ac10aa9
abhijeetAUR <abhijeetr131@gmail.com>
2019-09-16 19:12:54 +0530

    Added resources

---
+++ grabbit/app/src/main/java/com/example/grabbit/SplashscreenActivity.kt
@@ -0,0 +1,24 @@
+package com.example.grabbit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.os.Handler
+class SplashscreenActivity : AppCompatActivity() {
+    val SPLASH_DELAY: Long = 3000 //3 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this,LoginActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_DELAY)
+    }
+}
